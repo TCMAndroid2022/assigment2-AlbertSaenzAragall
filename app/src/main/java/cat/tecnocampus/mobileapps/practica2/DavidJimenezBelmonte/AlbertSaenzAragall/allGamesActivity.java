1c09b613d8f88cd2a85a5ee9f12d4070e8a52c64
David Jimenez Belmonte <d.jimenez.belmonte@gmail.com>
2022-06-05 12:34:47 +0200

    cosos

---
+++ app/src/main/java/cat/tecnocampus/mobileapps/practica2/DavidJimenezBelmonte/AlbertSaenzAragall/allGamesActivity.java
@@ -0,0 +1,14 @@
+package cat.tecnocampus.mobileapps.practica2.DavidJimenezBelmonte.AlbertSaenzAragall;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class allGamesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_games);
+    }
+}
